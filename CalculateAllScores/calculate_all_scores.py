c793b5555031a5f4868ccd1091089e803658e88a
nick hannay <nickhannay@outlook.com>
2023-12-02 15:49:44 -0800

    seach results now limited to only nhl players

--- CalculateAllScores/calculate_all_scores.py
+++ CalculateAllScores/calculate_all_scores.py
@@ -1,3 +1,8 @@
+'''
+    This module is used to calculate
+    the HOF score for any player
+    who has played at least 1 NHL game
+'''
 import concurrent.futures
 import json
 import math
@@ -12,6 +17,11 @@ scores = []
 
 
 def get_all_players():
+    '''Returns all players in the NHL database
+        Returns:
+            players (List[dict]): A list of all players where each element is a [dict] containing the name and id
+            numPlayers (int): The number of players returned (# players in NHL DB)
+    '''
     params = {'limit': '25000', 'q': '*', 'culture': 'en-us'}
     response = requests.get(SEARCH_URL, params=params, timeout=20)
     players = json.loads(response.text)
@@ -19,6 +29,15 @@ def get_all_players():
 
 
 def get_awards(player):
+    '''
+        Returns the amount of awards won by 'player'
+
+        Parameters:
+            player (dict): the json response containing all 'player' info casted to a (dict)
+
+        Returns:
+            awards (dict): the number of each award won by the player
+    '''
     awards = {
         'selke': 0,
         'hart': 0,
@@ -62,12 +81,20 @@ def get_awards(player):
 
 
 def calculate_hof_scores(player):
+    '''
+        Returns the HOF score for 'player'
+
+        Parameters:
+            player (dict): The JSON response for 'player' casted to a dict using 'json.loads()'
+
+        Returns:
+            hof_score (int): the HOF score calculated from 'player' stats
+    '''
     hof_score = 0
     if 'careerTotals' not in player:
         return hof_score
     if 'regularSeason' not in player['careerTotals']:
         return hof_score
-
     awards = get_awards(player)
     regular_season = player['careerTotals']['regularSeason']
     playoffs = player['careerTotals']['playoffs'] if 'playoffs' in player['careerTotals'] else {'points': 0}
@@ -115,7 +142,23 @@ def calculate_hof_scores(player):
     return hof_score
 
 
-def calculate_chunk_scores(players, start, end, scores):
+def calculate_chunk_scores(players, start, end):
+    '''
+        A thread function that writes the score for each player
+        in players[start, end) into scores[i], where i is in [start, end).
+        [start, end) is the chunkSize for which each thread is responsible for
+
+        Parameters:
+            players (List[dict]):
+                A list where each element is a dict containing player info
+            start (int):
+                The start index for the sub array to be processed by the THREAD
+            end (int):
+                The end index to stop processing for each THREAD
+            scores: (List):
+                The global list for which each thread writes its results to
+    '''
+
     for i in range(start, end):
         if players[i]['lastSeasonId'] is None:
             continue
@@ -126,29 +169,36 @@ def calculate_chunk_scores(players, start, end, scores):
             scores[i] = f'{players[i]['name']},{score}\n'
 
 
-if __name__ == '__main__':
-    allPlayers, numPlayers = get_all_players()
-    scores = [None] * numPlayers
+def main():
+    '''
+    The main entry point for this module
+    '''
+    all_players, num_players = get_all_players()
+    scores = [None] * num_players
     NUM_THREADS = 10
-    chunkSize = math.ceil(numPlayers / NUM_THREADS)
+    chunk_size = math.ceil(num_players / NUM_THREADS)
     futures = []
 
     with open(RESULTS_FILE, 'w', encoding='utf-8') as file:
         file.write('Player, Score\n')
 
-    print(numPlayers)
-    startTime = time.time()
+    print(f'Number of Players: {num_players}')
+    start_time = time.time()
     with concurrent.futures.ThreadPoolExecutor(max_workers=NUM_THREADS) as executor:
-        for i in range(0, numPlayers, chunkSize):
-            end = min(i + chunkSize, numPlayers)
+        for i in range(0, num_players, chunk_size):
+            end = min(i + chunk_size, num_players)
             futures.append(
-                executor.submit(calculate_chunk_scores, allPlayers, i, end, scores)
+                executor.submit(calculate_chunk_scores, all_players, i, end)
             )
         concurrent.futures.wait(futures)
-    endTime = time.time()
-    print(f'elapsed time: {endTime - startTime}')
+    end_time = time.time()
+    print(f'elapsed time: {end_time - start_time}')
 
     with open(RESULTS_FILE, 'a', encoding='utf-8') as file:
         for score in scores:
             if score is not None:
                 file.write(score)
+
+
+if __name__ == '__main__':
+    main()
--- src/routes/searchPlayer.js
+++ src/routes/searchPlayer.js
@@ -2,7 +2,8 @@ const express = require('express')
 
 const router = express.Router()
 
-const BASE_URL = "https://search.d3.nhle.com/api/v1/search/player"
+const SEARCH_URL = "https://search.d3.nhle.com/api/v1/search/player"
+const BASE_URL = 'https://api-web.nhle.com/v1/player/'
 
 router.post('/', async (req, res) => {
         const name = req.body["search-input"]
@@ -10,13 +11,7 @@ router.post('/', async (req, res) => {
 
         
         // make get request to external API
-        const playerResults = await getSearchResults(name, 25)
-        /*const nhlPlayers = playerResults.filter(player => [
-                player.hasOwnProperty('careerTotals')
-        ])
-
-
-        console.log(`Results: ${nhlPlayers}`)*/
+        const playerResults = await getSearchResults(name)
         res.render('index', { results : playerResults, searchTerm: name});
 })
 
@@ -24,27 +19,35 @@ router.post('/', async (req, res) => {
 
 
 
-async function getSearchResults(searchName, limit){
+async function getSearchResults(searchName){
         let players = []
         if(searchName){
                 const searchParams = {
-                        limit: limit,
                         culture: 'en-us',
                         q: searchName
                 }
         
-                const searchUrl = new URL(BASE_URL)
+                const searchUrl = new URL(SEARCH_URL)
                 searchUrl.search = new URLSearchParams(searchParams).toString()
                 const searchResults = await fetch(searchUrl)
                 const resultsJSON = await searchResults.json()
+                console.log(resultsJSON)
+        
+                await Promise.all(resultsJSON.map(async (player) => {
+                    if (player.lastSeasonId !== null) {
+                      try {
+                        const headShotResponse = await fetch(BASE_URL + player.playerId + '/landing');
+                        const playerDetails = await headShotResponse.json();
+                        const headShotSrc = playerDetails.headshot; 
+                        players.push({ name: player.name, id: player.playerId, headShot: headShotSrc });
 
-        
-                
-                resultsJSON.forEach( (player) => {
-                        players.push({name: player.name, id: player.playerId})
-                })
+                      } catch (error) {
+                        console.error(`Error retrieving player headshots: ${error}`);
+                      }
+                    }
+                  }));
         }
-        
+        console.log(players)
         return players
 
 }
